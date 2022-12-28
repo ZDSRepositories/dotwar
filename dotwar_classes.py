589c0446ff20951586278f9913ad913cc99ce444
ZDSRepositories <89665494+ZDSRepositories@users.noreply.github.com>
2022-12-27 22:22:06 -0600

    removed testing code from dotwar_classes.py

--- dotwar_classes.py
+++ dotwar_classes.py
@@ -317,31 +317,3 @@ class Game:
 			events = self.system["event_log"]
 
 		return events
-
-
-if __name__ == "__main__":  # test code
-	g = Game("TESTGAME", "C:\\Users\\1zada\\PycharmProjects\\dotwar")
-	g.new(overwrite=True)
-	g.load()
-
-	g.add_entity("TEST1", "ADMIN", [0, 0, 0], [0, 0, 0], [0.1, 0, 0], "craft", [], 0, True)
-	g.add_entity("TEST2", "ADMIN", [10, 10, 10], [0, 0, 0], [2, 2, 2], "craft", [], 1, True)
-	g.add_entity("Earth", None, [0, 0, 0], [0, 0, 0], [0, 0, 0], "planet", [], -1)
-	g.edit_entity("Earth", "captured", False)
-	# g.save()
-	# g.load()
-
-	"""g.add_order("TEST1", task="burn", time=(g.system["game"]["system_time"] + datetime.timedelta(hours=1)), args={"a":[0.1, 0.0, 0.0]})
-	g.add_order("TEST1", task="burn", time=(g.system["game"]["system_time"] + datetime.timedelta(hours=3)), args={"a":[0.0, -0.1, 0.0]})
-	#print("pending orders:",g.get_pending())
-	#print(g.update(25))
-	g.update(25/10)"""
-	g.add_order("TEST1", "burn", {"a": [1, 0, 0]}, time=(g.system["game"]["system_time"] + datetime.timedelta(hours=1)))
-	# print("pending orders:",g.get_pending())
-	# print("TEST1 velocity:",g.get_entity("TEST1")["v"])
-	g.save()
-	# print("event log:")
-	print("Events:")
-	[print(" ", event) for event in g.system["event_log"]]
-	print("Pending orders:")
-	[print(order) for order in g.get_pending("TEST1")]
